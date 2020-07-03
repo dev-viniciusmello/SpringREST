42874d2d5bc86a87dfa383d9c9cd394d4d6c3357
vinicius-tx <vinicius.pro.contact@gmail.com>
2020-07-03 18:01:31 -0300

    Metodo para salvar usuario

--- src/main/java/com/viniciusmello/springrest/controller/IndexController.java
+++ src/main/java/com/viniciusmello/springrest/controller/IndexController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,17 +22,27 @@ public class IndexController {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
-	@GetMapping(value = "/{id}/codigodavenda/{venda}", produces = "application/json")
-	public ResponseEntity<Usuario> relatorio(@PathVariable("id") Long id, 
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> init(@PathVariable("id") Long id, 
 			@PathVariable(value = "venda") Long venda) {
-		System.out.println(id);
-		System.out.println(venda);
-		return new ResponseEntity<Usuario>(usuarioRepository.findById(id).get(), HttpStatus.OK);	
+		
+		return new ResponseEntity<Usuario>(usuarioRepository.findById(id).get(), 
+				HttpStatus.OK);	
+		
 	}
 	
 	@GetMapping(value = "/", produces = "application/json")
 	public ResponseEntity<List<Usuario>> usuarios() {
 		return new ResponseEntity<List<Usuario>>((List<Usuario>) usuarioRepository.findAll(), HttpStatus.OK);
 	}
+	
+	
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrarVenda(@RequestBody Usuario usuario) {
+		System.out.println("Entrou");
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK); 
+	}
+	
 	
 }

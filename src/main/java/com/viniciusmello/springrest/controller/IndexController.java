b5cbcd1e2352c10d7174351fb0318e0e47141bec
vinicius-tx <vinicius.pro.contact@gmail.com>
2020-07-03 15:34:09 -0300

    Testes para entender o funcionamento do spring REST

--- src/main/java/com/viniciusmello/springrest/controller/IndexController.java
+++ src/main/java/com/viniciusmello/springrest/controller/IndexController.java
@@ -4,15 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController 
 @RequestMapping(value = "/usuario")
 public class IndexController {
 
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<String> init() {
-		return new ResponseEntity<String>("Ola usuario Rest Spring Boot", HttpStatus.OK);
+	public ResponseEntity<String> init(@RequestParam(value = "nome", defaultValue = "NomeNaoInformado") String nome,
+			@RequestParam(value = "salario", required = false) String salario){
+		System.out.println("Parametro sendo recebido: " + nome);
+		return new ResponseEntity<String>("Ola usuario Rest Spring Boot, seu nome é : " + nome +  " Salario = "+ salario, HttpStatus.OK);
 	}
 	
 	

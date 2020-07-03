df5952af7cc11691c98cc74db4e3c9a6ab67d619
vinicius-tx <vinicius.pro.contact@gmail.com>
2020-07-03 14:06:01 -0300

    Configurando projeto Spring Rest

--- src/main/java/com/viniciusmello/springrest/CursospringrestapiApplication.java
+++ src/main/java/com/viniciusmello/springrest/CursospringrestapiApplication.java
@@ -1,9 +1,23 @@
 package com.viniciusmello.springrest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EntityScan(basePackages = {"com.viniciusmello.springrest.model"})
+@ComponentScan(basePackages = {"curso.*"}) 
+@EnableJpaRepositories(basePackages = {"com.viniciusmello.springrest.repository"})
+@EnableTransactionManagement 
+@EnableWebMvc
+@RestController
 @SpringBootApplication
+@EnableAutoConfiguration
 public class CursospringrestapiApplication {
 
 	public static void main(String[] args) {

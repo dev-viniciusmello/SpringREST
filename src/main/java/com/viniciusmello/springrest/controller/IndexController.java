17e63a558d587509e0b3370b1497c6d3eacfdb6c
vinicius-tx <vinicius.pro.contact@gmail.com>
2020-07-03 15:49:29 -0300

    Retornando json List<Usuario> Teste

--- src/main/java/com/viniciusmello/springrest/controller/IndexController.java
+++ src/main/java/com/viniciusmello/springrest/controller/IndexController.java
@@ -1,8 +1,10 @@
 package com.viniciusmello.springrest.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +17,20 @@ import com.viniciusmello.springrest.model.Usuario;
 public class IndexController {
 
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> init() {
-		Usuario usuario = new Usuario();
-		usuario.setLogin("dev.viniciusmello@gmail.com");
-		usuario.setNome("Marcus Vinicius");
-		usuario.setSenha("1121212323232");		
-		return ResponseEntity.ok(usuario);
+	public ResponseEntity<List<Usuario>>init() {
+
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		
+		for (Long i = 0l; i < 30 ; i++) {
+			Usuario usuario = new Usuario();
+			usuario.setId(i);
+			usuario.setLogin("dev.viniciusmello@gmail.com");
+			usuario.setNome("Marcus Vinicius");
+			usuario.setSenha("1121212323232");
+			usuarios.add(usuario);
+		}
+		
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
 	
 	}
 	

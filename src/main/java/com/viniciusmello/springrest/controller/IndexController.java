2671f7244e2df8276ddd5a8d11b53a58b1e91b0b
vinicius-tx <vinicius.pro.contact@gmail.com>
2020-07-03 19:04:15 -0300

    Exemplo de delete com deleteMapping

--- src/main/java/com/viniciusmello/springrest/controller/IndexController.java
+++ src/main/java/com/viniciusmello/springrest/controller/IndexController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,10 +48,15 @@ public class IndexController {
 	
 	@PutMapping(value = "/") // Atualizar
 	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
-		// Fazendo alguma coisa antes de atualizar ou salvar.
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK); 
 		
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		usuarioRepository.deleteById(id);
+		return "ok";
 	}
 	
 	

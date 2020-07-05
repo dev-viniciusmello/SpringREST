f69df3ed003e2dbec0a2ddf917ea0f982b110626
vinicius-tx <vinicius.pro.contact@gmail.com>
2020-07-05 12:37:43 -0300

    Adicionando anotation entidade

--- src/main/java/com/viniciusmello/springrest/model/Telefone.java
+++ src/main/java/com/viniciusmello/springrest/model/Telefone.java
@@ -1,12 +1,15 @@
 package com.viniciusmello.springrest.model;
 
 import java.io.Serializable;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class Telefone implements Serializable {
 	
 	private static final long serialVersionUID = 1L;

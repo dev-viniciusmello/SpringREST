d99477044c9185cba53066635262316388771470
vinicius-tx <vinicius.pro.contact@gmail.com>
2020-07-06 06:38:47 -0300

    Cross Origin - Controle de acesso das nossas APIs

--- src/main/java/com/viniciusmello/springrest/controller/IndexController.java
+++ src/main/java/com/viniciusmello/springrest/controller/IndexController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.viniciusmello.springrest.model.Telefone;
 import com.viniciusmello.springrest.model.Usuario;
 import com.viniciusmello.springrest.repository.UsuarioRepository;
 
+@CrossOrigin(origins = "*") // Todas as requisições vão passar a funcionar
 @RestController 
 public class IndexController {
 	
